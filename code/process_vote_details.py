cbc815fea782187fca99ee391d6821912a5da258
Mark Newman <mark@trinetteandmark.com>
2020-08-11 17:08:26 -0400

    remove debug code

--- code/process_vote_details.py
+++ code/process_vote_details.py
@@ -88,8 +88,7 @@ if __name__ == '__main__':
         help = 'CSV file containing the aggregated data',
         type = pathlib.Path,
         required = True)
-    #args = parser.parse_args()    
-    #print(f'folder in: {args.folder_in}')
-    #print(f'file out: {args.file_out}')
-    #process_vote_details(args.folder_in, args.file_out)
-    process_vote_details(pathlib.Path('d:/temp/vote_details'), pathlib.Path('d:/temp/vote_details.csv'))
+    args = parser.parse_args()    
+    print(f'folder in: {args.folder_in}')
+    print(f'file out: {args.file_out}')
+    process_vote_details(args.folder_in, args.file_out)
